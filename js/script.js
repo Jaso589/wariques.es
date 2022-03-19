451e37b2ee758511b06b091ea74819afb840cf6d
Jaso589 <84824469+Jaso589@users.noreply.github.com>
2022-03-18 22:39:56 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/* ********** Menu ********** */
+
+/******* slide ******** */
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},4000);
+function muestraSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('miSlider');
+    let barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
+  
+/***********menu *********** */
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn"),
+      $menu = d.querySelector(".menu"),
+      $menuText = d.querySelector(".menu-mobil-op");
+  
+    $btnMenu.addEventListener("click", (e) => {
+        $btnMenu.firstElementChild.classList.toggle("none");
+        $btnMenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("is-active");
+        $menuText.classList.toggle("is-active")
+
+    });
+  
+    d.addEventListener("click", (e) => {
+      if (!e.target.matches(".menu a")) return false;
+  
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+      $menuText.classList.remove("is-active");
+    });
+  })(document);
+
